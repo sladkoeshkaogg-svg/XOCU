a8206223000aa977392d74b1c9300dab129a360e
sladkoeshkaogg-svg <sladkoeshkaogg@gmail.com>
2026-08-24 17:35:21 +0500

    Update XOCU.lua

--- XOCU.lua
+++ XOCU.lua
@@ -4,6 +4,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local CoreGui = game:GetService("CoreGui")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+local LocalPlayer = Players.LocalPlayer
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sladkoeshkaogg-svg/XOCU/refs/heads/main/XOCU%20FAKELIBRORY"))()
 
 Players = game:GetService('Players')
